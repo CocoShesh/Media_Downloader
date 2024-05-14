742caddf1f0ec47981328e5a168e47b4638dc3c7
NARD <glenardrefamonte1@gmail.com>
2024-05-14 14:51:09 +0800

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useDownloader from "react-use-downloader";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { size, elapsed, percentage, download, cancel, error, isInProgress } =
+    useDownloader();
+
+  const fileUrl =
+    "https://scontent.fath3-4.fna.fbcdn.net/v/t39.30808-6/441263542_875331677975423_6530867776316508376_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5f2048&_nc_ohc=m1rweqXkGccQ7kNvgE7LTum&_nc_ht=scontent.fath3-4.fna&oh=00_AYB8u5J_MC98x2dSUDO2AOe7F61OAZotu9ctCwybzlxJBA&oe=6648F476&dl=1&dl=1";
+
+  const filename = "image.png";
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="App">
+        <p>Download is in {isInProgress ? "in progress" : "stopped"}</p>
+        <button onClick={() => download(fileUrl, filename)}>
+          Click to download the file
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => cancel()}>Cancel the download</button>
+        <p>Download size in bytes {size}</p>
+        <label>Downloading progress:</label>
+        <progress id="file" value={percentage} max="100" />
+        <p>Elapsed time in seconds {elapsed}</p>
+        {error && <p>possible error {JSON.stringify(error)}</p>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
