5c0e816316917e76ace076e16f2f994a9813a953
NARD <glenardrefamonte1@gmail.com>
2024-05-14 17:57:00 +0800

    Updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import Navbar from "../Navbar";
-import { useTheme } from "../../context/ThemeContext";
+import Toggle from "../Toggle";
+
 const Header = () => {
-  const { handleChangeTheme, theme } = useTheme();
   return (
     <>
       <header className="w-full px-10  py-7 flex items-center justify-between 2xl:w-[1500px]">
@@ -11,16 +11,7 @@ const Header = () => {
           </h1>
         </section>
         <Navbar />
-        <section className="w-full flex items-end justify-end">
-          <section className="h-7 w-12 relative rounded-full bg-[#ee8c1e]">
-            <div
-              onClick={handleChangeTheme}
-              className={`absolute h-5 w-5 cursor-pointer transition-transform duration-300 ease-in-out  bg-[white] rounded-full top-1  ${
-                theme ? "translate-x-6" : "translate-x-1"
-              }`}
-            ></div>
-          </section>
-        </section>
+        <Toggle />
       </header>
     </>
   );
