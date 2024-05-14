6750e87029de892824e4adba319acbfd718e9ff8
NARD <glenardrefamonte1@gmail.com>
2024-05-14 17:15:33 +0800

    Updated

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <>
+      <ul className="flex items-center  justify-center gap-5 font-semibold cursor-pointer text-lg w-full">
+        <li className="hover:text-[#e05f1e]">About</li>
+        <li className="hover:text-[#e05f1e]">FAQ</li>
+        <li className="hover:text-[#e05f1e]">How it works</li>
+        <li className="hover:text-[#e05f1e]">Features</li>
+      </ul>
+    </>
+  );
+};
+
+export default Navbar;
