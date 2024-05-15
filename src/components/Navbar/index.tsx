020b766951617604ce4db38dc6e8a0c958e32916
NARD <glenardrefamonte1@gmail.com>
2024-05-16 01:07:54 +0800

    Updated

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <>
-      <ul className="flex items-center  justify-center gap-5 font-semibold cursor-pointer text-lg w-full">
+      <ul className="flex items-center  justify-center gap-5 font-semibold cursor-pointer text-lg text-black w-full max-xl:hidden">
         <li className="hover:text-[#e05f1e]">About</li>
         <li className="hover:text-[#e05f1e]">FAQ</li>
         <li className="hover:text-[#e05f1e]">How it works</li>
