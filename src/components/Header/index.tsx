ea5bf4a93d1d3fa1196f7f6eeda5769313d1908f
NARD <glenardrefamonte1@gmail.com>
2024-05-16 01:07:48 +0800

    Updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,9 @@
 import Navbar from "../Navbar";
 import Toggle from "../Toggle";
-
 const Header = () => {
   return (
     <>
-      <header className="w-full px-10  py-7 flex items-center justify-between 2xl:w-[1500px]">
+      <header className="w-full px-10  py-7 flex items-center justify-between 2xl:w-[1500px] max-lg:px-2">
         <section className="w-full">
           <h1 className="font-bold text-2xl  text-[#230056] ">
             Soc<span className="text-[#e05f1e]">Downloader</span>
