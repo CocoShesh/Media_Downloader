ee70f0a6d30d1b0e727e36dccea455b39f88f974
NARD <glenardrefamonte1@gmail.com>
2024-05-17 04:04:09 +0800

    Updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,11 @@ const Header = () => {
     <>
       <header className="w-full px-10  py-7 flex items-center justify-between 2xl:w-[1500px] max-lg:px-2">
         <section className="w-full">
-          <h1 className="font-bold text-2xl  text-[#230056] ">
-            Soc<span className="text-[#e05f1e]">Downloader</span>
-          </h1>
+          <a href="/">
+            <h1 className="font-bold text-2xl  text-[#230056] ">
+              Soc<span className="text-[#e05f1e]">Downloader</span>
+            </h1>{" "}
+          </a>
         </section>
         <Navbar />
         <Toggle />
