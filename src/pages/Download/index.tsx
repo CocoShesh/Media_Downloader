cf077b5b3dc2832732ec153e59aef8aee90ba28a
NARD <glenardrefamonte1@gmail.com>
2024-05-17 05:22:46 +0800

    Updated

--- src/pages/Download/index.tsx
+++ src/pages/Download/index.tsx
@@ -1,32 +1,25 @@
 import { useDownloader } from "../../context/DownloaderContext";
+
 const Download = () => {
   const { data } = useDownloader();
+
   return (
-    <>
-      <section className="h-[500px] card flex flex-col items-center justify-center w-full px-64  2xl:w-[1500px] max-lg:px-5  mt-5 rounded-2xl ">
-        <section className="w-fit px-7 py-1 h-8 rounded-2xl mb-5 bg-white text-black">
-          Best in industry
-        </section>
-        <h1 className="text-white text-5xl w-[700px] max-md:w-full text-center max-sm:text-4xl ">
-          Download High-quality videos and images from different social media
-        </h1>
-        <section>
-          {/* {data.map(
-            (item, index) =>
-              item.response.url && (
-                <a
-                  key={index}
-                  href={item.response.url}
-                  download
-                  className="w-fit px-7 py-1 h-8 rounded-2xl mb-5 bg-white text-black"
-                >
-                  {item.response.resolution}
-                </a>
-              )
-          )} */}
-        </section>
+    <section className="h-[500px] card flex flex-col items-center justify-center w-full px-64 2xl:w-[1500px] max-lg:px-5 mt-5 rounded-2xl">
+      <section className="w-fit px-7 py-1 h-8 rounded-2xl mb-5 bg-white text-black">
+        Best in industry
       </section>
-    </>
+      <h1 className="text-white text-5xl w-[700px] max-md:w-full text-center max-sm:text-4xl">
+        Download High-quality videos and images from different social media
+      </h1>
+      <div>
+        <h2>{data?.title}</h2>
+        <img
+          src={data?.picture}
+          alt={data?.title}
+          className="w-[150px] h-[150px]"
+        />
+      </div>
+    </section>
   );
 };
 
