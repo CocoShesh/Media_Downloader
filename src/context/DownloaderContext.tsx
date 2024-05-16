a88950a5ca3288bd9a54a186db9af2191a83c84a
NARD <glenardrefamonte1@gmail.com>
2024-05-17 04:04:21 +0800

    Updated

--- src/context/DownloaderContext.tsx
+++ src/context/DownloaderContext.tsx
@@ -1,8 +1,16 @@
 import { useContext, createContext, useState, ReactNode } from "react";
 
+type DownloadData = {
+  response: {
+    resolution: string;
+    url: string;
+  };
+};
 type DownloaderContextType = {
   selectedType: string;
   setSelectedType: React.Dispatch<React.SetStateAction<string>>;
+  data: DownloadData | null;
+  setData: React.Dispatch<React.SetStateAction<DownloadData | null>>;
 };
 
 type DownloaderProviderProps = {
@@ -12,6 +20,8 @@ type DownloaderProviderProps = {
 const DownloaderContext = createContext<DownloaderContextType>({
   selectedType: "",
   setSelectedType: () => {},
+  data: null,
+  setData: () => {},
 });
 
 export const useDownloader = () => {
@@ -20,9 +30,12 @@ export const useDownloader = () => {
 
 export const DownloaderProvider = ({ children }: DownloaderProviderProps) => {
   const [selectedType, setSelectedType] = useState("");
+  const [data, setData] = useState<DownloadData | null>(null);
 
   return (
-    <DownloaderContext.Provider value={{ selectedType, setSelectedType }}>
+    <DownloaderContext.Provider
+      value={{ selectedType, setSelectedType, setData, data }}
+    >
       {children}
     </DownloaderContext.Provider>
   );
