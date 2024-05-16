42386205bb707d0e213701e5849219224a7a13b4
NARD <glenardrefamonte1@gmail.com>
2024-05-17 04:22:42 +0800

    Updated

--- src/pages/Download/index.tsx
+++ src/pages/Download/index.tsx
@@ -1,4 +1,6 @@
+import { useDownloader } from "../../context/DownloaderContext";
 const Download = () => {
+  const { data } = useDownloader();
   return (
     <>
       <section className="h-[500px] card flex flex-col items-center justify-center w-full px-64  2xl:w-[1500px] max-lg:px-5  mt-5 rounded-2xl ">
@@ -8,6 +10,21 @@ const Download = () => {
         <h1 className="text-white text-5xl w-[700px] max-md:w-full text-center max-sm:text-4xl ">
           Download High-quality videos and images from different social media
         </h1>
+        <section>
+          {/* {data.map(
+            (item, index) =>
+              item.response.url && (
+                <a
+                  key={index}
+                  href={item.response.url}
+                  download
+                  className="w-fit px-7 py-1 h-8 rounded-2xl mb-5 bg-white text-black"
+                >
+                  {item.response.resolution}
+                </a>
+              )
+          )} */}
+        </section>
       </section>
     </>
   );
