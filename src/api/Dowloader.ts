fd40cd89a57ece38409cc00783421e1ed6baf86d
NARD <glenardrefamonte1@gmail.com>
2024-05-18 17:39:27 +0800

    Updated

--- src/api/Dowloader.ts
+++ src/api/Dowloader.ts
@@ -1,19 +1,20 @@
 import axios from "axios";
 const apiKey: string = import.meta.env.VITE_APP_X_RAPIDAPI_KEY as string;
+
 export const Downloader = async (url: string) => {
   try {
     const options = {
-      method: "POST",
-      url: "https://social-download-all-in-one.p.rapidapi.com/v1/social/autolink",
-      headers: {
-        "content-type": "application/json",
-        "X-RapidAPI-Key": apiKey,
-        "X-RapidAPI-Host": "social-download-all-in-one.p.rapidapi.com ",
-      },
-      data: {
+      method: "GET",
+      url: "https://facebook-video-and-reel-downloader.p.rapidapi.com/",
+      params: {
         url: url,
       },
+      headers: {
+        "X-RapidAPI-Key": apiKey,
+        "X-RapidAPI-Host": "facebook-video-and-reel-downloader.p.rapidapi.com",
+      },
     };
+
     const response = await axios.request(options);
     return response.data;
   } catch (error) {
