f11649cc63b2969478a84c07263f0b469152cf01
NARD <glenardrefamonte1@gmail.com>
2024-05-18 15:27:40 +0800

    color for the dark mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ const Header = () => {
       <header className="w-full px-10  py-7 flex items-center justify-between 2xl:w-[1500px] max-lg:px-2">
         <section className="w-full">
           <a href="/">
-            <h1 className="font-bold text-2xl  text-[#230056] ">
+            <h1 className="font-bold text-2xl  text-[#230056] dark:text-white ">
               Soc<span className="text-[#e05f1e]">Downloader</span>
             </h1>{" "}
           </a>
