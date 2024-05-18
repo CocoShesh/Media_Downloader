5c3b46faf871316dfb7f4b9f25a7f25fb70de9a6
NARD <glenardrefamonte1@gmail.com>
2024-05-18 15:31:18 +0800

    Remove background and background image styling configuration
    This change removes the configuration related to background and background image styling to streamline the CSS and avoid redundant styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,7 @@ export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      backgroundColor: {
-        "custom-color": "hsla(291, 85%, 25%, 1)",
-      },
-      backgroundImage: {
-        "custom-bg": `
-          radial-gradient(
-            at 28% 78%,
-            hsla(280, 88%, 24%, 1) 0px,
-            transparent 50%
-          ),
-          radial-gradient(
-            at 95% 79%,
-            hsla(262, 96%, 12%, 1) 0px,
-            transparent 50%
-          )
-        `,
-      },
-    },
+    extend: {},
   },
   plugins: [require("daisyui")],
 };
