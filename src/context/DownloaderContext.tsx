32ef087d710a4e6179d06a1455923a7c47be117e
NARD <glenardrefamonte1@gmail.com>
2024-06-16 01:56:38 +0800

    Updated

--- src/context/DownloaderContext.tsx
+++ src/context/DownloaderContext.tsx
@@ -1,46 +1,5 @@
-import {
-  createContext,
-  useContext,
-  useState,
-  ReactNode,
-  Dispatch,
-  SetStateAction,
-} from "react";
-
-type PhotoData = {
-  type: string;
-  url: string;
-};
-
-type VideoData = {
-  videoVariants: {
-    content_type: string;
-    url: string;
-    bitrate: number;
-  }[];
-};
-
-type UserData = {
-  description: string;
-  screen_name: string;
-  profile: string;
-};
-
-export type MediaData = {
-  user: UserData;
-  created_at: string;
-  media: {
-    video: VideoData | null;
-    photo: PhotoData[] | null;
-  };
-};
-
-type DownloaderContextType = {
-  selectedType: string;
-  setSelectedType: Dispatch<SetStateAction<string>>;
-  data: MediaData | null;
-  setData: Dispatch<SetStateAction<MediaData | null>>;
-};
+import { createContext, useContext, useState, ReactNode } from "react";
+import { MediaData, DownloaderContextType } from "../utils/Types";
 
 type DownloaderProviderProps = {
   children: ReactNode;
