583a4f4252a06881be60a52e69306e4c5411100f
NARD <glenardrefamonte1@gmail.com>
2024-06-16 02:17:11 +0800

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-// import useDownloader from "react-use-downloader";
-
 import Header from "./components/Header";
 import MainContent from "./components/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TwitterDownload from "./pages/TwitterDownload";
+import FacebookDownload from "./pages/FacebookDownload";
+import InstagramDownload from "./pages/InstagramDownload";
+import TiktokDownload from "./pages/TiktokDownload";
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
           <Routes>
             <Route path="/" element={<MainContent />} />
             <Route path="/Download" element={<TwitterDownload />} />
+            <Route path="/Facebook" element={<FacebookDownload />} />
+            <Route path="/Instagram" element={<InstagramDownload />} />
+            <Route path="/Tiktok" element={<TiktokDownload />} />
           </Routes>
         </BrowserRouter>
       </main>
