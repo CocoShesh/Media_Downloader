727d09756e05c2a97c82536af93c436b86d00a24
NARD <glenardrefamonte1@gmail.com>
2024-06-16 02:03:34 +0800

    Updated

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,10 +2,10 @@ const Navbar = () => {
   return (
     <>
       <ul className="flex items-center  justify-center gap-5 font-semibold  dark:text-white cursor-pointer text-lg text-black w-full max-xl:hidden">
-        <li className="hover:text-[#e05f1e]">About</li>
-        <li className="hover:text-[#e05f1e]">FAQ</li>
-        <li className="hover:text-[#e05f1e]">How it works</li>
-        <li className="hover:text-[#e05f1e]">Features</li>
+        <li className="hover:text-[#e05f1e]">Twitter</li>
+        <li className="hover:text-[#e05f1e]">Facebook</li>
+        <li className="hover:text-[#e05f1e]">Instagram</li>
+        <li className="hover:text-[#e05f1e]">Tiktok</li>
       </ul>
     </>
   );
