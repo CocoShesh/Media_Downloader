bab19daeeafedc01c95028a6947fa435e9488e8a
NARD <glenardrefamonte1@gmail.com>
2024-06-16 02:58:36 +0800

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ import TiktokDownload from "./pages/TiktokDownload";
 function App() {
   return (
     <>
-      <main className="min-h-screen flex flex-col items-center justify-center  dark:bg-[#000000]  transform  transition-all duration-300 ease-in-out bg-white px-7 pb-20 max-lg:px-3 ">
-        <Header />
-        <BrowserRouter>
+      <BrowserRouter>
+        <main className="min-h-screen flex flex-col items-center justify-center  dark:bg-[#000000]  transform  transition-all duration-300 ease-in-out bg-white px-7 pb-20 max-lg:px-3 ">
+          <Header />
           <Routes>
             <Route path="/" element={<MainContent />} />
             <Route path="/Download" element={<TwitterDownload />} />
@@ -19,8 +19,8 @@ function App() {
             <Route path="/Instagram" element={<InstagramDownload />} />
             <Route path="/Tiktok" element={<TiktokDownload />} />
           </Routes>
-        </BrowserRouter>
-      </main>
+        </main>
+      </BrowserRouter>
     </>
   );
 }
