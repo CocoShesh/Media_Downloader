8ac50e74fd485465adb1c234b14fc1b35e57dd29
NARD <glenardrefamonte1@gmail.com>
2024-06-16 01:52:08 +0800

    Updated

--- src/api/Dowloader.ts
+++ src/api/Dowloader.ts
@@ -22,10 +22,29 @@ export const TwitterDownloader = async (url: string): Promise<MediaData> => {
     const response = await axios.request(options);
     console.log(response.data);
 
-    // Ensure response.data matches MediaData type
     return response.data as MediaData;
   } catch (error) {
     console.error(error);
-    throw error; // Re-throw error after logging it
+    throw error;
+  }
+};
+
+export const FacebookDownloader = async (url: string): Promise<MediaData> => {
+  try {
+    const options = {
+      method: "GET",
+      url: "https://facebook-reel-and-video-downloader.p.rapidapi.com/app/main.php",
+      params: {
+        url: url,
+      },
+      headers: {
+        "x-rapidapi-key": "91afa38e15mshec1dc2b2813aae7p1f2fdcjsn016b4889ccb8",
+        "x-rapidapi-host": "facebook-reel-and-video-downloader.p.rapidapi.com",
+      },
+    };
+    const response = await axios.request(options);
+    return response.data as MediaData;
+  } catch (error) {
+    console.error(error);
   }
 };
