26a0f0c1361a1db82a35fdc248827f8bb8b843d2
NARD <glenardrefamonte1@gmail.com>
2024-06-17 03:19:10 +0800

    Updated

--- src/api/Dowloader.ts
+++ src/api/Dowloader.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const apiKey: string = import.meta.env.VITE_APP_X_RAPIDAPI_KEY as string;
-import { MediaData, FacebookData } from "../utils/Types";
 
-export const TwitterDownloader = async (url: string): Promise<MediaData> => {
+export const TwitterDownloader = async (url: string) => {
   try {
     const options = {
       method: "POST",
@@ -22,16 +21,14 @@ export const TwitterDownloader = async (url: string): Promise<MediaData> => {
     const response = await axios.request(options);
     console.log(response.data);
 
-    return response.data as MediaData;
+    return response.data;
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
 
-export const FacebookDownloader = async (
-  url: string
-): Promise<FacebookData> => {
+export const FacebookDownloader = async (url: string) => {
   try {
     const options = {
       method: "GET",
@@ -45,7 +42,7 @@ export const FacebookDownloader = async (
       },
     };
     const response = await axios.request(options);
-    return response.data as FacebookData;
+    return response.data;
   } catch (error) {
     console.error(error);
     throw error;
@@ -88,6 +85,28 @@ export const TiktokDownloader = async (url: string) => {
           "tiktok-downloader-download-tiktok-videos-without-watermark.p.rapidapi.com",
       },
     };
+    const response = await axios.request(options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const YoutubeDownloader = async (url: string) => {
+  try {
+    const options = {
+      method: "GET",
+      url: "https://social-media-video-downloader.p.rapidapi.com/smvd/get/youtube",
+      params: {
+        url: url,
+      },
+      headers: {
+        "x-rapidapi-key": apiKey,
+        "x-rapidapi-host": "social-media-video-downloader.p.rapidapi.com",
+      },
+    };
+
     const response = await axios.request(options);
     return response.data;
   } catch (error) {
