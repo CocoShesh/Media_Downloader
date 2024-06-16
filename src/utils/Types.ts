ea7e38c047f6919bb834c55e187265af4eb9bed0
NARD <glenardrefamonte1@gmail.com>
2024-06-17 00:28:55 +0800

    Updated

--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
+
 interface PhotoData {
   type: string;
   url: string;
@@ -10,6 +11,7 @@ interface VideoData {
     bitrate: number;
   }[];
 }
+
 interface UserData {
   description: string;
   screen_name: string;
@@ -18,6 +20,8 @@ interface UserData {
 
 export interface MediaData {
   user: UserData;
+  error: string;
+  errors: string;
   created_at: string;
   media: {
     video: VideoData | null;
@@ -30,4 +34,43 @@ export interface DownloaderContextType {
   setSelectedType: Dispatch<SetStateAction<string>>;
   data: MediaData | null;
   setData: Dispatch<SetStateAction<MediaData | null>>;
+  fData: FacebookData | null;
+  setFData: Dispatch<SetStateAction<FacebookData | null>>;
+  instaData: ImageItem | null;
+  setInstaData: Dispatch<SetStateAction<ImageItem | null>>;
+  setTiktokData: Dispatch<SetStateAction<TwitterData | null>>;
+  tiktokData: TwitterData | null;
+}
+
+export interface FacebookData {
+  title: string;
+  thumbnail: string;
+  links: {
+    DownloadLowQuality: string;
+    DownloadHighQuality: string;
+  }[];
+  media: {
+    sd_url: string;
+    hd_url: string;
+    type: string;
+    image: string;
+  }[];
+  error: string;
+  errors: string;
+}
+
+export interface ImageItem {
+  posts: {
+    type: string;
+    url: string;
+    thumbnail: string;
+  }[];
+}
+
+export interface TwitterData {
+  video: string;
+  music: string;
+  cover: string;
+  description: string;
+  OriginalWatermarkedVideo: string;
 }
