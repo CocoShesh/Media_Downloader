c919c0cba421c11d23715592eb852aaccfe9bca2
NARD <glenardrefamonte1@gmail.com>
2024-06-17 00:28:34 +0800

    Updated

--- src/context/DownloaderContext.tsx
+++ src/context/DownloaderContext.tsx
@@ -1,5 +1,11 @@
 import { createContext, useContext, useState, ReactNode } from "react";
-import { MediaData, DownloaderContextType } from "../utils/Types";
+import {
+  MediaData,
+  DownloaderContextType,
+  FacebookData,
+  ImageItem,
+  TwitterData,
+} from "../utils/Types";
 
 type DownloaderProviderProps = {
   children: ReactNode;
@@ -14,10 +20,23 @@ export const useDownloader = () => useContext(DownloaderContext);
 export const DownloaderProvider = ({ children }: DownloaderProviderProps) => {
   const [selectedType, setSelectedType] = useState("");
   const [data, setData] = useState<MediaData | null>(null);
-
+  const [fData, setFData] = useState<FacebookData | null>(null);
+  const [instaData, setInstaData] = useState<ImageItem | null>(null);
+  const [tiktokData, setTiktokData] = useState<TwitterData | null>(null);
   return (
     <DownloaderContext.Provider
-      value={{ selectedType, setSelectedType, data, setData }}
+      value={{
+        selectedType,
+        setSelectedType,
+        data,
+        setData,
+        setFData,
+        fData,
+        instaData,
+        setInstaData,
+        setTiktokData,
+        tiktokData,
+      }}
     >
       {children}
     </DownloaderContext.Provider>
