bbf803c03d6176a746f4b26b91ff8672f792b1ae
NARD <glenardrefamonte1@gmail.com>
2024-06-18 01:08:05 +0800

    Updated

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,5 +1,12 @@
 const Loading = () => {
-  return <div>index</div>;
+  return (
+    <div className="text-white absolute bg-[#f2f3f6]   z-10 w-full flex max-md:rounded-md  flex-col items-center justify-center  h-full rounded-full top-0 left-0  ">
+      <div className="loader  ">
+        <span>SocDownloader</span>
+        <span>SocDownloader</span>
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
