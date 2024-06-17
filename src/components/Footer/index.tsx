ca9a5e29297acbe6a521f362e66541b74a00d1b6
NARD <glenardrefamonte1@gmail.com>
2024-06-18 01:03:57 +0800

    Updated

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className=" bg-white  text-black dark:bg-black dark:text-white py-5 fixed bottom-0 left-0 right-0">
+      <div className=" mx-auto px-4">
+        <div className="text-center ">
+          <p>&copy; 2024 Nard. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
