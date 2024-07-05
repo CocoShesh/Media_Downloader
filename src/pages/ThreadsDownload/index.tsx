2fcc92fe725f0a743193f57f1759e569b14f1838
NARD <glenardrefamonte1@gmail.com>
2024-07-05 23:59:17 +0800

    Updated

--- src/pages/ThreadsDownload/index.tsx
+++ src/pages/ThreadsDownload/index.tsx
@@ -1,10 +1,12 @@
 import { useDownloader as useDownloaderContext } from "../../context/DownloaderContext";
 import useDownloader from "react-use-downloader";
 import { handleSelectedItem } from "../../utils/LinkUtils";
+import { useState } from "react";
 
 const ThreadsDownload = () => {
   const { threadsData } = useDownloaderContext();
-  const { download } = useDownloader();
+  const { download, isInProgress, percentage } = useDownloader();
+  const [mediaIndex, setMediaIndex] = useState<number>(0);
 
   const imageUrls = threadsData?.result.image_urls || [];
   const videoUrls = threadsData?.result.video_urls || [];
@@ -16,9 +18,9 @@ const ThreadsDownload = () => {
   return (
     <>
       <section className="flex flex-col items-center justify-center gap-5">
-        <div className=" gap-5  mt-5  flex items-center justify-center  w-[800px] flex-wrap">
+        <div className="gap-5 mt-5 flex items-center justify-center w-[800px] flex-wrap">
           {allUrls.map((media, index) => (
-            <div key={index} className="relative ">
+            <div key={index} className="relative">
               {media.type === "video" ? (
                 <video
                   src={media.url}
@@ -33,6 +35,7 @@ const ThreadsDownload = () => {
               )}
               <button
                 onClick={() => {
+                  setMediaIndex(index);
                   handleSelectedItem(
                     download,
                     media.url,
@@ -43,8 +46,23 @@ const ThreadsDownload = () => {
                 }}
                 className="absolute bottom-0 bg-orange-500 w-full h-12 text-xl text-white rounded-b-md"
               >
-                Download
+                {mediaIndex === index && isInProgress
+                  ? "Downloading"
+                  : "Download"}
               </button>
+              {mediaIndex === index && isInProgress && (
+                <>
+                  <span className="absolute bottom-14 left-1 text-white text-lg">
+                    Percentage
+                  </span>
+                  <progress
+                    className="absolute bottom-10 w-full left-0"
+                    id="file"
+                    value={percentage}
+                    max="100"
+                  />
+                </>
+              )}
             </div>
           ))}
         </div>
