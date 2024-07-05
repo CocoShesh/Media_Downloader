6a8a278e6df37889f390a110af57d76ea5d0f13a
NARD <glenardrefamonte1@gmail.com>
2024-07-05 14:57:24 +0800

    updated

--- src/pages/FacebookDownload/index.tsx
+++ src/pages/FacebookDownload/index.tsx
@@ -1,9 +1,13 @@
 import { useDownloader as useDownloaderContext } from "../../context/DownloaderContext";
 import { handleSelectedItem } from "../../utils/LinkUtils";
 import useDownloader from "react-use-downloader";
+import { useState } from "react";
 const FacebookDownload = () => {
   const { fData } = useDownloaderContext();
-  const { download } = useDownloader();
+  const { download, percentage, isInProgress } = useDownloader();
+
+  const [mediaIndex, setMediaIndex] = useState<number>(0);
+
   return (
     <>
       <section className="flex items-center justify-center gap-5">
@@ -18,19 +22,33 @@ const FacebookDownload = () => {
                 className="h-[350px] w-full  rounded-md bg-white object-cover"
               />
               <button
-                onClick={() =>
+                onClick={() => {
+                  setMediaIndex(index);
                   handleSelectedItem(
                     download,
                     media.hd_url || media.sd_url || media.image,
                     `${fData.title}_${fData?.title}.${
                       media.type === "Photo" ? "png" : "mp4"
                     }`
-                  )
-                }
+                  );
+                }}
                 className="absolute bottom-0 bg-orange-500 w-full h-12 text-xl text-white rounded-b-md"
               >
-                Download
+                {isInProgress ? "Downloading " : "Download"}
               </button>
+              {mediaIndex === index && isInProgress && (
+                <>
+                  <span className="absolute bottom-14 left-1 text-white text-lg ">
+                    Percentage
+                  </span>
+                  <progress
+                    className="absolute bottom-10 w-full left-0 "
+                    id="file"
+                    value={percentage}
+                    max="100"
+                  />
+                </>
+              )}
             </section>
           ))}
         </section>
