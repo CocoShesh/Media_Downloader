9371de9ca82f79aa98a610b8429a44eb7dc40d78
NARD <glenardrefamonte1@gmail.com>
2024-07-05 14:57:27 +0800

    updated

--- src/pages/InstagramDownload/index.tsx
+++ src/pages/InstagramDownload/index.tsx
@@ -1,10 +1,12 @@
 import { useDownloader as useDownloaderContext } from "../../context/DownloaderContext";
 import useDownloader from "react-use-downloader";
 import { handleSelectedItem } from "../../utils/LinkUtils";
+import { useState } from "react";
 const InstagramDownload = () => {
   const { instaData } = useDownloaderContext();
-  const { download } = useDownloader();
-
+  const { download, percentage, isInProgress } = useDownloader();
+  const [imageIndex, setImageIndex] = useState<number>(0);
+  const [videoIndex, setVideoIndex] = useState<number>(0);
   return (
     <section className="flex items-center justify-center gap-5">
       <div className=" flex-col gap-5 grid grid-cols-2 mt-5">
@@ -19,6 +21,7 @@ const InstagramDownload = () => {
                 />
                 <button
                   onClick={() => {
+                    setImageIndex(index);
                     handleSelectedItem(
                       download,
                       `${image}`,
@@ -27,11 +30,27 @@ const InstagramDownload = () => {
                   }}
                   className="absolute bottom-0 bg-orange-500 w-full h-12 text-xl text-white rounded-b-md"
                 >
-                  Download
+                  {isInProgress ? "Downloading " : "Download"}
                 </button>
+                {imageIndex === index && isInProgress && (
+                  <>
+                    <span className="absolute bottom-14 left-1 text-white text-lg ">
+                      {" "}
+                      Percentage
+                    </span>
+
+                    <progress
+                      className="absolute bottom-10 w-full left-0 "
+                      id="file"
+                      value={percentage}
+                      max="100"
+                    />
+                  </>
+                )}
               </div>
             );
           })}
+
         {instaData?.links &&
           instaData?.links?.length > 0 &&
           instaData?.links.map((video, index) => {
@@ -47,6 +66,7 @@ const InstagramDownload = () => {
                 </span>
                 <button
                   onClick={() => {
+                    setVideoIndex(index);
                     const extension = video.quality === "hd_0" ? "mp4" : "m4a";
                     handleSelectedItem(
                       download,
@@ -56,8 +76,22 @@ const InstagramDownload = () => {
                   }}
                   className="absolute bottom-0 bg-orange-500 w-full h-12 text-xl text-white rounded-b-md"
                 >
-                  Download
+                  {isInProgress ? "Downloading " : "Download"}
                 </button>
+                {videoIndex === index && isInProgress && (
+                  <>
+                    <span className="absolute bottom-14 left-1 text-white text-lg ">
+                      {" "}
+                      Percentage
+                    </span>
+                    <progress
+                      className="absolute bottom-10 w-full left-0 "
+                      id="file"
+                      value={percentage}
+                      max="100"
+                    />
+                  </>
+                )}
               </div>
             );
           })}
