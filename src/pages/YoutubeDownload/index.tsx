f3b3ec67cf24268e423497e26df200339f77e2bd
NARD <glenardrefamonte1@gmail.com>
2024-07-07 12:59:11 +0800

    Updated

--- src/pages/YoutubeDownload/index.tsx
+++ src/pages/YoutubeDownload/index.tsx
@@ -28,7 +28,7 @@ const YoutubeDownload = () => {
                       video?.quality === "audio" ? "mp3" : "mp4";
                     handleSelectedItem(
                       download,
-                      `${video?.link}`,
+                      "https://rr2---sn-p5qddn76.googlevideo.com/videoplayback?expire=1720349613&ei=TR-KZpquC4qBkucPo92Z-AU&ip=54.81.182.196&id=o-AGadJQtMtt-vRW3NnzcLlbvf09cKTGxbmcNmnCvcQ7Jr&itag=313&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C271%2C278%2C313%2C394%2C395%2C396%2C397%2C398%2C399%2C400%2C401&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=my&mm=31%2C26&mn=sn-p5qddn76%2Csn-vgqsknzd&ms=au%2Conr&mv=u&mvi=2&pl=23&bui=AXc671KECEWuutIVf60J6YEH_7VuSSromQwTDI20_KJ803TOT9V42ajtqaBKxmSFCEm8-8Cf9aDUd3bI&spc=NO7bAQggwZLPGVqfRvta9j7rw8xJA8BMUShWvfSglTY2FDDcAg2ZCJJpj78b&vprv=1&svpuc=1&mime=video%2Fwebm&ns=DXZqWceuwwDnoi8WzmbMWFcQ&rqh=1&gir=yes&clen=492698563&dur=282.782&lmt=1710698414942301&mt=1720326512&fvip=1&keepalive=yes&c=WEB&sefc=1&txp=4532434&n=Ypw6hgkh2oY3fQ&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl&lsig=AHlkHjAwRQIgbzGdMjAevh9e0yt80icyujmi5cnPZuVZta_g7jbWXXsCIQDS-PHrAG0WGmdBhoEpsLvwJq5KjWZ5m4gkHzEWH8LKxw%3D%3D&sig=AJfQdSswRgIhANyZvXln2XmG8dLGhPKhml1I2EVigrqZ5ugSIevF3A1gAiEAj34lCdnnVFwN_ON1-DIyL8Av-IdadFdM1UJ60B6-mPk%3D",
                       `${ytData?.title}.${extension}`
                     );
                   }}
