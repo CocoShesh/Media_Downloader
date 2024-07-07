e0fa692a99a90bc97f5675fcb1bf09e9e0784384
NARD <glenardrefamonte1@gmail.com>
2024-07-07 12:59:00 +0800

    Updated

--- src/api/Dowloader.ts
+++ src/api/Dowloader.ts
@@ -92,27 +92,27 @@ export const TiktokDownloader = async (url: string) => {
   }
 };
 
-export const YoutubeDownloader = async (url: string) => {
-  try {
-    const options = {
-      method: "GET",
-      url: "https://social-media-video-downloader.p.rapidapi.com/smvd/get/youtube",
-      params: {
-        url: url,
-      },
-      headers: {
-        "x-rapidapi-key": apiKey,
-        "x-rapidapi-host": "social-media-video-downloader.p.rapidapi.com",
-      },
-    };
+// export const YoutubeDownloader = async (url: string) => {
+//   try {
+//     const options = {
+//       method: "GET",
+//       url: "https://social-media-video-downloader.p.rapidapi.com/smvd/get/youtube",
+//       params: {
+//         url: url,
+//       },
+//       headers: {
+//         "x-rapidapi-key": apiKey,
+//         "x-rapidapi-host": "social-media-video-downloader.p.rapidapi.com",
+//       },
+//     };
 
-    const response = await axios.request(options);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
+//     const response = await axios.request(options);
+//     return response.data;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
 
 export const ThreadsDownloader = async (url: string) => {
   try {
